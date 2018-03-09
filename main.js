73b51191506baa35f92356711c737f11fe074150
Ceci <thececiliaaa@panpan.attlocal.net>
2018-03-09 01:15:16 -0800

    add pics and music

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //initialize - set up variables
 var counter = document.getElementById('counter');
 var person = document.getElementById('person');
+var audio = document.getElementById('mySound');
 var fromTop = window.pageYOffset;
 
-counter.innerHTML = 'HOME';
-
+counter.innerHTML = 'Hello!';
 
 
 //pseudocode - when the user is scrolling update the variable fromTop and make decisions to change stuff on the page
@@ -20,41 +20,50 @@ window.onscroll = function (){
     counter.style.top = 50 + fromTop + "px";
 
     if (fromTop < 1000){
-      counter.innerHTML = "HOME";
-      //counter.style.backgroundImage = "url(counter.svg)";
+      counter.innerHTML = "Hello!";
+      music.style.backgroundImage = "none";
       person.style.backgroundImage = "none";
+      audio.src = "none";
     } if (fromTop > 1000){
-      counter.innerHTML = "Rock";
-      //counter.style.backgroundColor = "red";
+      counter.innerHTML = "Rock?";
+      music.style.backgroundImage = "none";
       person.style.backgroundImage = "none";
+      audio.src = "none";
     }if (fromTop > 2000){
       counter.innerHTML = "The 50's";
-      //counter.style.backgroundColor = "orange";
+      music.style.backgroundImage = "url(musicbg.svg)";
       person.style.backgroundImage = "url(elvis.svg)";
+      audio.src = "elvis.ogg";
     } if (fromTop > 3000){
       counter.innerHTML = "The 60's";
-      //counter.style.backgroundColor = "yellow";
+      music.style.backgroundImage = "url(musicbg.svg)";
       person.style.backgroundImage = "url(johnlennon.svg)";
+      audio.src = "beatles.ogg";
     } if (fromTop > 4000){
       counter.innerHTML = "The 70's";
-      //counter.style.backgroundColor = "green";
+      music.style.backgroundImage = "url(musicbg.svg)";
       person.style.backgroundImage = "url(ledzeppelin.svg)";
+      audio.src = "ledzeppelin.ogg";
     } if (fromTop > 5000){
       counter.innerHTML = "The 80's";
-      //counter.style.backgroundColor = "skyblue";
+      music.style.backgroundImage = "url(musicbg.svg)";
       person.style.backgroundImage = "url(gunsnroses.svg)";
+      audio.src = "gunsnroses.ogg";
     } if (fromTop > 6000){
       counter.innerHTML = "The 90's";
-      //counter.style.backgroundColor = "blue";
+      music.style.backgroundImage = "url(musicbg.svg)";
       person.style.backgroundImage = "url(nirvana.svg)";
+      audio.src = "nirvana.ogg";
     } if (fromTop > 7000){
       counter.innerHTML = "2000's";
-      //counter.style.backgroundColor = "violet";
+      music.style.backgroundImage = "url(musicbg.svg)";
       person.style.backgroundImage = "url(linkinpark.svg)";
+      audio.src = "linkinpark.ogg";
     } if (fromTop > 8000){
       counter.innerHTML = "NOW";
-      //counter.style.backgroundColor = "brown";
+      music.style.backgroundImage = "url(musicbg.svg)";
       person.style.backgroundImage = "url(twentyonepilots.svg)";
+      audio.src = "twentyonepilots.ogg";
     }
 }
 
@@ -90,3 +99,19 @@ function parallaxbubbles(){
 window.addEventListener('scroll', function(){ // on page scroll
  requestAnimationFrame(parallaxbubbles) // call parallaxbubbles() on next available screen paint
 }, false)
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
