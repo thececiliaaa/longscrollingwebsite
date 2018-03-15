695552589aedfd0599f4ce4782b87aa61b15cc24
Ceci <thececiliaaa@panpan.attlocal.net>
2018-03-14 23:38:45 -0700

    back to top

--- main.js
+++ main.js
@@ -78,11 +78,19 @@ var bgbanner = document.getElementById('bg-banner')
 var bgbanner2 = document.getElementById('bg-banner2')
 var rockinfo = document.getElementById('rockinfo')
 var fiftybg = document.getElementById('fiftybg')
+var fiftyinfo = document.getElementById('fiftyinfo')
 var sixtybg = document.getElementById('sixtybg')
+var sixtyinfo = document.getElementById('sixtyinfo')
 var seventybg = document.getElementById('seventybg')
+var seventyinfo = document.getElementById('seventyinfo')
 var eightybg = document.getElementById('eightybg')
+var eightyinfo = document.getElementById('eightyinfo')
 var ninetybg = document.getElementById('ninetybg')
+var ninetyinfo = document.getElementById('ninetyinfo')
 var oobg = document.getElementById('oobg')
+var ooinfo = document.getElementById('ooinfo')
+var currentbg = document.getElementById('currentbg')
+var currentinfo = document.getElementById('currentinfo')
 
 function parallaxbubbles(){
  var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
@@ -90,11 +98,19 @@ function parallaxbubbles(){
  bgbanner2.style.top = -scrolltop * .2 + 450 + 'px'
  rockinfo.style.top = -scrolltop * .2 + 1700 + 'px'
  fiftybg.style.top = -scrolltop * .2 + 2900 + 'px'
+ fiftyinfo.style.top = -scrolltop * .3 + 2800 + 'px'
  sixtybg.style.top = -scrolltop * .2 + 3900 + 'px'
+ sixtyinfo.style.top = -scrolltop * .3 + 4050 + 'px'
  seventybg.style.top = -scrolltop * .2 + 5200 + 'px'
+ seventyinfo.style.top = -scrolltop * .3 + 5300 + 'px'
  eightybg.style.top = -scrolltop * .2 + 6300 + 'px'
+ eightyinfo.style.top = -scrolltop * .3 + 6600 + 'px'
  ninetybg.style.top = -scrolltop * .2 + 7600 + 'px'
- oobg.style.top = -scrolltop * .2 + 8900 + 'px' // move bubble2 at 50% of scroll rate
+ ninetyinfo.style.top = -scrolltop * .3 + 7900 + 'px'
+ oobg.style.top = -scrolltop * .2 + 8850 + 'px'
+ ooinfo.style.top = -scrolltop * .3 + 9200 + 'px'
+ currentbg.style.top = -scrolltop * .2 + 10250 + 'px'
+ currentinfo.style.top = -scrolltop * .3 + 10700 + 'px' // move bubble2 at 50% of scroll rate
 }
 
 window.addEventListener('scroll', function(){ // on page scroll
@@ -115,4 +131,13 @@ function StopSound(soundobj) {
     var thissound=document.getElementById(soundobj);
     thissound.pause();
     thissound.currentTime = 0;
+}
+
+var timeOut;
+function scrollToTop() {
+	if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+		window.scrollBy(0,-50);
+		timeOut=setTimeout('scrollToTop()',10);
+	}
+	else clearTimeout(timeOut);
 }
